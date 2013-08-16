8440ec1d1bcb70a4d87d34b9de5df981181ce199
Muhaimin Ahsan <leroux@fezrev.com>
2013-08-16 02:28:21 -0400

    Redefine structures. Unbounded grid w/o wrapping. Still buggy b/c of out of bound grid checks.

--- src/GameOfLife/Game.hs
+++ src/GameOfLife/Game.hs
@@ -23,9 +23,12 @@ instance Show Cell where
 ----
 type Grid = M.Map Position Cell
 
+gridToList :: Grid -> [[Cell]]
+gridToList = transpose . chunksOf 50 . map snd . M.toAscList . M.filterWithKey inShownGrid
+  where inShownGrid (x, y) _ = x `elem` [1..50] && y `elem` [1..100]
+
 instance Show Grid where
-  show g = (unlines . map (intersperse ' ' . map (head . show)) . transpose . chunksOf m . map snd . M.toAscList) g where
-    m = (fst . fst . M.findMax) g
+  show = unlines . map (intersperse ' ' . map (head . show)) . gridToList
 
 -- Create grid of size (m * n) and populate with Cell c.
 initGrid :: Cell -> Int -> Int -> Grid
@@ -44,8 +47,8 @@ insertStructureWith :: Cell -> Structure -> Grid -> Grid
 insertStructureWith c s g = foldr (\k -> M.insert k c) g s
  
 -- Insert alive cells structure into grid.
-insertStructure :: Structure -> Grid -> Grid
-insertStructure = insertStructureWith Alive
+insertStructureAt :: Position -> Structure -> Grid -> Grid
+insertStructureAt p s = insertStructureWith Alive $ addToPosition s p
 
 isDead :: Grid -> Bool
 isDead = M.null . M.filter (== Alive)
@@ -58,10 +61,15 @@ isAlive = not . isDead
 ----
 -- Number of alive neighbors of a position.
 neighborsAlive :: Position -> Grid -> Int
-neighborsAlive p g = length $ filter (== Alive) $ catMaybes [M.lookup n g | n <- surrounding p]
+neighborsAlive p g = length $ filter (== Alive) $ getNeighbors p g
 
 getNeighbors :: Position -> Grid -> [Cell]
 getNeighbors p g = catMaybes [M.lookup n g | n <- surrounding p]
+
+unbound :: Structure -> Grid -> Structure
+unbound s g = map around s
+  where around (x, y) = (x `mod` mx, y `mod` my)
+        (mx, my) = fst $ M.findMax g
 
 -- Surrounding positions of some position.
 -- Should returns a list of 8 Positions.
@@ -93,19 +101,27 @@ tick g = M.mapWithKey (\k v -> step k v g) g
 ----
 -- Seeds
 ----
-blinker :: Position -> Structure
-blinker (x, y) = zip (repeat x) (take 3 $ iterate succ y)
+blinker :: Structure
+blinker = zip (repeat 0) [0..3]
 
 addToPosition :: Structure -> Position -> Structure
 addToPosition s (x, y) = map (\(a, b) -> (x + a, y + b)) s
 
-glider :: Position -> Structure
-glider = addToPosition glider' where
-  glider' = [(1, 0), (2, 1), (0, 2), (1, 2), (2, 2)]
+glider :: Structure
+glider = [(1, 0), (2, 1), (0, 2), (1, 2), (2, 2)]
 
-brilliant :: Position -> Structure
-brilliant = addToPosition brilliant' where
-  brilliant' = [(1, 0), (3, 1), (0, 2), (1, 2), (4, 2), (5, 2), (6, 2)]
+brilliant :: Structure
+brilliant = [(1, 0), (3, 1), (0, 2), (1, 2), (4, 2), (5, 2), (6, 2)]
+
+line :: Structure
+line = zip [1..10] (repeat 0)
+
+infinite :: Structure
+infinite = [(0, 0), (1, 0), (2, 0), (4, 0),
+            (0, 1),
+            (3, 2), (4, 2),
+            (1, 3), (2, 3), (4, 3),
+            (0, 4), (2, 4), (4, 4)]
 
 provisionGrid :: Grid
-provisionGrid = insertStructure (brilliant (25, 25)) (deadGrid 50 50)
+provisionGrid = insertStructureAt (25, 25) infinite (deadGrid 50 50)
